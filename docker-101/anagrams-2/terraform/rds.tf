58f9e159507d9512351800fecf714984f73dd309
Nick Zhu <nz3424@princeton.edu>
2026-07-09 13:18:51 -0400

    Add RDS MySQL instance for anagrams-2

---
+++ docker-101/anagrams-2/terraform/rds.tf
@@ -0,0 +1,31 @@
+resource "aws_db_subnet_group" "main" {
+  name       = "${var.project_name}-db-subnet-group"
+  subnet_ids = aws_subnet.public[*].id
+
+  tags = {
+    Name = "${var.project_name}-db-subnet-group"
+  }
+}
+
+resource "aws_db_instance" "main" {
+  identifier     = "${var.project_name}-db"
+  engine         = "mysql"
+  engine_version = var.db_engine_version
+
+  instance_class    = var.db_instance_class
+  allocated_storage = var.db_allocated_storage
+
+  db_name  = var.db_name
+  username = var.db_username
+  password = random_password.db_password.result
+
+  db_subnet_group_name   = aws_db_subnet_group.main.name
+  vpc_security_group_ids = [aws_security_group.rds.id]
+
+  skip_final_snapshot = true
+  publicly_accessible = false
+
+  tags = {
+    Name = "${var.project_name}-db"
+  }
+}
